5c7cd82a641d0d64d3b1ab960f730b2acc7ecffb
nickeita <nt89@njit.edu>
2021-08-02 14:53:20 -0400

    Task: Edited project to match Step I. in the tutorial and build docker image with MySQL db

--- app/app.py
+++ app/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, make_response, request, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
-# mysql = MySQL(cursorclass=DictCursor)
+mysql = MySQL(cursorclass=DictCursor)
 
-# app.config['MYSQL_DATABASE_HOST'] = 'db'
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-# app.config['MYSQL_DATABASE_PORT'] = 3306
-# app.config['MYSQL_DATABASE_DB'] = 'housingPrices'
-# mysql.init_app(app)
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'housingPrices'
+mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
@@ -18,14 +19,14 @@ def home():
     return render_template('home.html')
 
 
-# @app.route('/table', methods=['GET'])
-# def table():
-#     if request.method != 'GET':
-#         return make_response('Malformed request', 400)
-#     cursor = mysql.get_db().cursor()
-#     cursor.execute('SELECT * FROM zillow')
-#     result = cursor.fetchall()
-#     return render_template('table.html', houses=result)
+@app.route('/table', methods=['GET'])
+def table():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM zillow')
+    result = cursor.fetchall()
+    return render_template('table.html', houses=result)
 
 
 if __name__ == '__main__':
