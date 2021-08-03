c37164be683cd31d033ae03e159d9c99c941f17a
nickeita <nt89@njit.edu>
2021-08-03 19:09:50 -0400

    Task: Completed step VI. of the tutorial.

---
+++ app/application/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_redis import Redis
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+
+redis = Redis()
+mysql = MySQL(cursorclass=DictCursor)
+
+
+def init_app():
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_pyfile('config.py')
+
+    mysql.init_app(app)
+    redis.init_app(app)
+
+    with app.app_context():
+        from . import routes
+
+        app.register_blueprint(auth.auth_bp)
+        app.register_blueprint(admin.admin_bp)
+
+        return app
+
+
+
+
